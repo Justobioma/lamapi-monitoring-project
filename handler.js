c168ada7621643345ed502dfce90647ac318f170
Justobioma <justobioma@outlook.com>
2025-10-10 10:16:00 +0100

    update handler and yml files to include /list endpoint

--- handler.js
+++ handler.js
@@ -1,48 +1,26 @@
-const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBDocumentClient, ScanCommand } = require("@aws-sdk/lib-dynamodb");
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-//const { v4: uuidv4 } = require("uuid");
-const { randomUUID } = require('crypto');
 
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
 
-module.exports.submit = async (event) => {
+module.exports.list = async () => {
   try {
-    const body = JSON.parse(event.body || '{}');
-    const { name, message } = body;
-
-    if (!name || !message) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'Missing name or message' }),
-      };
-    }
-
-    const item = {
-     // id: uuidv4(),
-      id: randomUUID(),
-      name,
-      message,
-      timestamp: new Date().toISOString(),
-    };
-
-    await docClient.send(new PutCommand({
+    const data = await docClient.send(new ScanCommand({
       TableName: 'SubmissionTable',
-      Item: item,
     }));
 
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        status: 'Stored successfully',
-        item,
-      }),
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: data.Items }),
     };
   } catch (err) {
-    console.error("Error storing submission:", err);
+    console.error("Error retrieving submissions:", err);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Internal server error' }),
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ error: "Internal server error" }),
     };
   }
 };
